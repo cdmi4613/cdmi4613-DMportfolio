fab9e3df95f88f7c6d930edcabb4b8ee88fbd1af
cdm <cdmi4613@naver.com>
2025-06-21 18:18:05 +0900

    25년6월버전업데이트

---
+++ subpage/js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function(){
+    let swiper=new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        // spaceBetween: 30,
+        // loop: true,
+        // loopedSlides: 1,
+        pagination: { 
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+         hide:false
+        },
+        
+        navigation: {
+        nextEl: ".swiper .controller ul li a.next",
+        prevEl: ".swiper .controller ul li a.prev",
+        },
+        
+        // autoplay: {
+        // delay: 2000,
+        //    },
+        breakpoints: { 
+            100:{
+                slidesPerView: 1,
+            },
+            800:{
+                slidesPerView: 2,
+            },
+            1300:{
+                slidesPerView: 3,
+            },
+        }
+        
+        });
+})
